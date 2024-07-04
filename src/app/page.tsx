0cfbd73672ff79d4b6a958e64f01bec4da3fb804
immanuelk1m <kse0119@naver.com>
2024-07-04 14:18:14 +0900

    add spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,55 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Search from "@/components/component/search";
 import ResultTable from "@/components/component/result-table";
+import Spiner from "@/components/component/spiner";
 import axios from 'axios';
+import '@/app/globals.css';
 
-function Page() {
+const Page: React.FC = () => {
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const resultTableRef = useRef<HTMLDivElement | null>(null);
 
-  const handleSearch = async (url : any) => 
-    {
-    try 
-    {
+  const handleSearch = async (url: string) => {
+    setLoading(true);
+    try {
       const response = await axios.post('http://localhost:5000/analyze', { video_url: url });
-      console.log(response.data);
       let jsonData;
-      if (typeof response.data === 'string') 
-      {
-          jsonData = JSON.parse(response.data.replace(/NaN/g, 'null'));
-      } 
-      else 
-      {
-          jsonData = response.data;
+      if (typeof response.data === 'string') {
+        jsonData = JSON.parse(response.data.replace(/NaN/g, 'null'));
+      } else {
+        jsonData = response.data;
       }
-      console.log(jsonData.results);
 
-      if (jsonData && jsonData.results) 
-      {
-        
-          setResults(jsonData.results); 
-      } 
-      
-    } 
-    catch (error) {
+      setResults(jsonData.results);
+      setLoading(false);
+    } catch (error) {
       console.error('Error fetching analysis results:', error);
+      setLoading(false);
     }
   };
+
+  useEffect(() => {
+    if (results.length > 0 && resultTableRef.current) {
+      resultTableRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [results]);
 
   return (
     <div>
       <Search onSearch={handleSearch} />
-      <ResultTable results={results} />
+      {loading && (
+        <div className="overlay">
+          <div className="spinner-container">
+            <Spiner />
+          </div>
+        </div>
+      )}
+      <div ref={resultTableRef}>
+        <ResultTable results={results} loading={loading} />
+      </div>
     </div>
   );
 }

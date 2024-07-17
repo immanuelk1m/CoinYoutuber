7d23f7d6485685bd74424543537ac03646306094
immanuelk1m <kse0119@naver.com>
2024-07-17 21:35:16 +0900

    update times

--- src/pages/api/stock.tsx
+++ src/pages/api/stock.tsx
@@ -15,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
 
       const response = await axios.post('http://34.22.90.37:5000/stock', req.body, {
-        timeout: 300000
+        timeout: 900000
       });
       console.log('Response data:', response.data);
       res.status(200).json(response.data);

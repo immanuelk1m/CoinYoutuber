a5e0e2a8a4a2ecc5a34c00c7737b59f5e359fd68
immanuelk1m <kse0119@naver.com>
2024-07-17 19:15:12 +0900

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: '/api/:path*',
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;

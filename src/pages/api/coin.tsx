59c428d77c062b294d981269a7d1b980edba1d56
immanuelk1m <kse0119@naver.com>
2024-07-17 20:15:40 +0900

    update coin.tsx

--- src/pages/api/coin.tsx
+++ src/pages/api/coin.tsx
@@ -5,13 +5,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     try {
       console.log('Request body:', req.body);
+      
+      const videoUrl = req.body.video_url;
+      const videoIdMatch = videoUrl.match(/(?:v=|\/)([0-9A-Za-z_-]{11}).*/);
+
+      if (!videoIdMatch) {
+        res.status(400).json({ error: 'Invalid video URL format' });
+        return;
+      }
+
       const response = await axios.post('http://34.22.90.37:5000/coin', req.body, {
         timeout: 120000
       });
       console.log('Response data:', response.data);
       res.status(200).json(response.data);
-    } catch (error) {
+    } catch (error:any) {
       console.error('Error fetching data:', error);
+      if (error.response) {
+        console.error('Error response data:', error.response.data);
+        console.error('Error response status:', error.response.status);
+        console.error('Error response headers:', error.response.headers);
+      }
       res.status(500).json({ error: 'An error occurred while fetching data' });
     }
   } else {

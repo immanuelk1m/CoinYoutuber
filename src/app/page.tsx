0480410baf990a0932a18bacc40cdc3221234123
immanuelk1m <kse0119@naver.com>
2024-07-17 12:44:44 +0900

    add head meta codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import ResultTable from "@/components/component/result-table";
 import Spiner from "@/components/component/spiner";
 import Header from "@/components/component/header";
 import Footer from "@/components/component/footer";
+import Head from 'next/head';
 
 import axios from 'axios';
 import '@/app/globals.css';
@@ -43,7 +44,27 @@ const Page: React.FC = () => {
   }, [results]);
 
   return (
-    <div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <title>Spready - 미국주식/코인 유튜브 분석</title>
+        <meta name="description" content="This is an awesome application built with Next.js" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="유튜브 코인 추천 영상의 실제 수익률을 분석하여 불법 리딩과 사기를 방지하는 도구입니다. 투자 결정에 도움을 주는 객관적인 데이터를 제공합니다."></meta>
+        <meta name="keywords" content="코인, 암호화폐, 유튜브, 투자, 리딩, 사기 방지, 수익률 분석, 투자 도구"></meta>
+        <meta name="author" content="당신의 이름 또는 회사명"></meta>
+        <meta property="og:title" content="코인 추천 유튜버 분석 도구"></meta>
+        <meta property="og:description" content="유튜브 코인 추천 영상의 실제 수익률을 분석하여 불법 리딩과 사기를 방지하는 도구입니다. 객관적인 데이터로 현명한 투자 결정을 도와드립니다."></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:title" content="코인 추천 유튜버 분석 도구"></meta>
+        <meta name="twitter:description" content="유튜브 코인 추천 영상의 실제 수익률을 분석하여 불법 리딩과 사기를 방지하는 도구입니다. 객관적인 데이터로 현명한 투자 결정을 도와드립니다."></meta>
+        <meta property="og:url" content="https://spready.kr"></meta>
+        <meta property="og:image" content="https://spready.kr/main.png"></meta>
+        <meta name="twitter:image" content="https://spready.kr/main.png"></meta>
+        <link rel="icon" href="/favicon.ico" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7656508177587264" crossOrigin="anonymous"></script>
+      </Head>
       <Header/>
       <Search onSearch={handleSearch} activeTab={activeTab} setActiveTab={setActiveTab} />
       {loading && (
@@ -57,8 +78,9 @@ const Page: React.FC = () => {
         <ResultTable results={results} loading={loading} />
       </div>
       <Footer/>
-    </div>
+    </>
   );
 }
 
 export default Page;
+
